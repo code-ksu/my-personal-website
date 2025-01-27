3f03154022da4beeda8edcd6601ffa6c778f352f
code-ksu <kseniia.teslenko@gmail.com>
2025-01-27 20:10:34 +0100

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Kseniia', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Kseniia Brauer/i);
   expect(linkElement).toBeInTheDocument();
 });
